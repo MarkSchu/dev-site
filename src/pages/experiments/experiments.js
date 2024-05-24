49796e0582d9f16961b499bb32e1da5c022e6f35
Mark Schumaker <m.schumaker235@gmail.com>
2024-05-23 20:17:35 -0400

    add drop-shadow tool

--- src/pages/experiments/experiments.js
+++ src/pages/experiments/experiments.js
@@ -10,25 +10,38 @@ export default function ExperimentsPage() {
         element('h1', {className: 'title', textContent: 'Experiments'}),
         element('p', {className: 'text', textContent: 'These are some of my experiments. I mostly play around with vanilla JavaScript and CSS. 🧪'}),
         element('div', {},
-            // element('div', {},
-            //     element('h4', {textContent: 'Todo App'}),
-            //     element('p', {textContent: "A todo app written in Vanilla JS. I wanted to build something that didn't use modern front-end frameworks."}),
-            //     element('a', {textContent: 'try here'})
-            // ),
+        
             element('div', {className: 'experiment-list'},
-                element('h4', {textContent: 'Dot Game'}),
-                element('p', {textContent: 'A game made with Vanilla JS, using svg animation.'}),
-                element('a', {
-                    textContent: 'play',
-                    href: 'https://cranky-williams-0031ff.netlify.app/'
-                }),
-                element('a', {
-                    textContent: 'code',
-                    href: 'https://github.com/MarkSchu/dot-game/tree/master'
-                })
+
+                element('div', {},
+                    element('h4', {textContent: 'Dot Game'}),
+                    element('p', {textContent: 'A game made with Vanilla JS, using svg animation.'}),
+                    element('a', {
+                        textContent: 'play',
+                        href: 'https://cranky-williams-0031ff.netlify.app/'
+                    }),
+                    element('a', {
+                        textContent: 'code',
+                        href: 'https://github.com/MarkSchu/dot-game/tree/master'
+                    })
+                ),
+
+                element('div', {},
+                    element('h4', {textContent: 'Dropshadow Tool'}),
+                    element('p', {textContent: 'A tool made with Vanilla JS that allows you to play around with the drop shadow effect on an svg.'}),
+                    element('a', {
+                        textContent: 'play',
+                        href: 'https://dropshadowtool.netlify.app/'
+                    }),
+                    element('a', {
+                        textContent: 'code',
+                        href: 'https://github.com/MarkSchu/drop-shadow-tool'
+                    })
+                )
             )
         )
       )
     )
   )
 }
+
