fd90ca5b1a8387827bd6a1205c58419622d69132
Mark Schumaker <markschumaker@marks-mbp.lan>
2024-06-26 11:08:04 -0400

    color scheme and content in place

--- src/index.js
+++ src/index.js
@@ -29,13 +29,19 @@ function repeat(list, createEl) {
   return list.map(item => createEl(item));
 }
 
+const skills = [
+  'JavaScript', 'React', 'TypeScript', 'Node', 'Jest', 'Express', 'Hapi', 'Redux', 
+  'Sass', 'Less', 'Webpack', 'GraphQL', 'MongoDB', 'SQL', 'CSS/HTML', 'Design Patterns', 
+  'RESTful APIs'
+];
+
 function App() {
   return (
     element('main', {},
       element('header', {},
-        element('h1', {textContent: 'Mark Schumaker'}),
+        element('h1', {innerHTML: 'Mark Schumaker'}),
         element('h2', {textContent: 'Frontend Engineer'}),
-        element('p', {textContent: 'I build apps and websites with JavaScript.'})
+        element('p', {textContent: 'I build apps and websites with JavaScript'})
       ),
       element('section', {},
         element('h2', {textContent: 'ABOUT'}),
@@ -44,13 +50,35 @@ function App() {
           a way to play around with the ideas. Eventually the hobby turned into a profession and I've 
           been doing it for 10 years.`}
         ),
-        element('p', {textContent: `My main focus is working on frontends in the JavaScript, particularly
-          with Node, React, Typescript`})
+        element('p', {textContent: `When I'm away from my computer, I like to spend time with my family and friends, 
+          read philosophy books, and hike outside.`})
+      ),
+
+      element('section', {},
+        element('h2', {textContent: 'WORK'}),
+        element('p', {innerHTML: `My main focus these days is building Node apps with React, TypeScript,
+          and other tools in the JavaScript Ecosystem. I've been doing that for 8 years. I'm passionate
+          about simple solutions, avoiding complexity, and writing code that is both readable and adaptable.`}
+        ),
+        element('p', {innerHTML: `I'm also passionate about learning and have experience with loads of other
+          technologies from Python to Haskell to WebGL. And I've spent a lot of time mulling over more abstract topics like
+          software architecture, design, and programming paradigms.`}
+        )
       ),
 
       element('section', {}, 
         element('h2', {textContent: 'SKILLS'}),
-        element('p', {textContent: `JavaScript, React, TypeScript, Node, Jest, Express, Hapi, Redux, Sass, Less, Webpack, GraphQL, MongoDB, SQL, CSS/HTML, Design Patterns, RESTful APIS`})
+        element('p', {textContent: `Here are a few technologies I know how to use.`}),
+        repeat(skills, (skill) =>
+          element('div', {className: 'pill', textContent: skill})
+        ),
+        element('p', {innerHTML: `On the <b>soft skill</b> side of things, I'm a good communicator, love to teach and 
+          explain ideas to others, strong in creative problem solving, adaptable, and can provide leadership to 
+          junior developers. I think my greatest strength, however, is that: <blockquote>I can learn anything.</blockquote>`}),
+      ),
+
+      element('section', {}, 
+        element('h2', {textContent: 'EXPERIENCE'}),
       )
     )
   )
