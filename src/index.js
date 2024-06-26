5fbe733cac9a19e939343e62e6a1299c0217567c
Mark Schumaker <markschumaker@marks-mbp.lan>
2024-06-26 13:49:04 -0400

    add nav menu

--- src/index.js
+++ src/index.js
@@ -25,60 +25,89 @@ function element(tag, attrs) {
   return el;
 }
 
-function repeat(list, createEl) {
-  return list.map(item => createEl(item));
+function repeat(tag, attrs, list, createEl) {
+  const el = element(tag, attrs);
+  list.map(item => 
+    el.appendChild(createEl(item))
+  );
+  return el;
 }
 
 const skills = [
-  'JavaScript', 'React', 'TypeScript', 'Node', 'Jest', 'Express', 'Hapi', 'Redux', 
+  'Frontend', 'Full Stack', 'JavaScript', 'React', 'TypeScript', 'Node', 'Jest', 'Express', 'Hapi', 'Redux', 
   'Sass', 'Less', 'Webpack', 'GraphQL', 'MongoDB', 'SQL', 'CSS/HTML', 'Design Patterns', 
-  'RESTful APIs'
+  'RESTful APIs', 'Responsive Design', 'A11y'
 ];
 
 function App() {
   return (
-    element('main', {},
-      element('header', {},
-        element('h1', {innerHTML: 'Mark Schumaker'}),
-        element('h2', {textContent: 'Frontend Engineer'}),
-        element('p', {textContent: 'I build apps and websites with JavaScript'})
+    element('div', {},
+      element('nav', {},
+        element('a', {
+          textContent: 'About',
+          href: '#about',
+        }),
+        element('a', {
+          textContent: 'Work',
+          href: '#work',
+        }),
+        element('a', {
+          textContent: 'Skills',
+          href: '#skills',
+        }),
+        element('a', {
+          textContent: 'Experience',
+          href: '#experience',
+        }),
+        element('a', {
+          className: 'resume',
+          textContent: 'Resume',
+          href: '',
+        })
       ),
-      element('section', {},
-        element('h2', {textContent: 'ABOUT'}),
-        element('p', {textContent: `I started coding through Philosophy. Back when I was working on my 
-          Masters, I thought a lot about logic and computation and picked up programming as 
-          a way to play around with the ideas. Eventually the hobby turned into a profession and I've 
-          been doing it for 10 years.`}
+      element('main', {},
+        element('header', {},
+          element('h1', {innerHTML: 'Mark Schumaker'}),
+          element('h2', {textContent: 'Frontend Engineer'}),
+          element('p', {textContent: 'I build apps and websites with JavaScript'})
         ),
-        element('p', {textContent: `When I'm away from my computer, I like to spend time with my family and friends, 
-          read philosophy books, and hike outside.`})
-      ),
+        element('section', {id: 'about'},
+          element('h2', {textContent: 'ABOUT'}),
+          element('p', {textContent: `I started coding through Philosophy. Back when I was working on my 
+            Masters, I thought a lot about logic and computation and picked up programming as 
+            a way to play around with the ideas. Eventually the hobby turned into a profession and I've 
+            been doing it for 10 years.`}
+          ),
+          element('p', {textContent: `When I'm away from my computer, I like to spend time with my family and friends, 
+            read philosophy books, and hike.`})
+        ),
 
-      element('section', {},
-        element('h2', {textContent: 'WORK'}),
-        element('p', {innerHTML: `My main focus these days is building Node apps with React, TypeScript,
-          and other tools in the JavaScript Ecosystem. I've been doing that for 8 years. I'm passionate
-          about simple solutions, avoiding complexity, and writing code that is both readable and adaptable.`}
+        element('section', {id: 'work'},
+          element('h2', {textContent: 'WORK'}),
+          element('p', {innerHTML: `My main focus these days is building Node apps with React, TypeScript,
+            and other tools in the JavaScript Ecosystem. I've been doing that for 8 years. I'm passionate
+            about simple solutions, avoiding complexity, and writing code that is both readable and adaptable.`}
+          ),
+          element('p', {innerHTML: `I'm also passionate about learning and have experience with loads of other
+            technologies from Python to Haskell to WebGL. And I've spent a lot of time mulling over more abstract topics like
+            software architecture, design, and programming paradigms.`}
+          )
         ),
-        element('p', {innerHTML: `I'm also passionate about learning and have experience with loads of other
-          technologies from Python to Haskell to WebGL. And I've spent a lot of time mulling over more abstract topics like
-          software architecture, design, and programming paradigms.`}
+
+        element('section', {id: 'skills'}, 
+          element('h2', {textContent: 'SKILLS'}),
+          element('p', {textContent: `Here are a few technical skills I have.`}),
+          repeat('div', {className: 'skill-list'}, skills, (skill) =>
+            element('div', {className: 'pill', textContent: skill})
+          ),
+          element('p', {innerHTML: `On the <b>soft skill</b> side of things, I'm a good communicator, a strong creative problem solver, 
+            love to teach and explain ideas to others, adaptable, and can provide leadership to 
+            junior developers. I think my greatest strength, however, is knowing how to learn. I feel like I can learn pretty much anything.`}),
+        ),
+
+        element('section', {id: 'experience'}, 
+          element('h2', {textContent: 'EXPERIENCE'}),
         )
-      ),
-
-      element('section', {}, 
-        element('h2', {textContent: 'SKILLS'}),
-        element('p', {textContent: `Here are a few technologies I know how to use.`}),
-        repeat(skills, (skill) =>
-          element('div', {className: 'pill', textContent: skill})
-        ),
-        element('p', {innerHTML: `On the <b>soft skill</b> side of things, I'm a good communicator, love to teach and 
-          explain ideas to others, strong in creative problem solving, adaptable, and can provide leadership to 
-          junior developers. I think my greatest strength, however, is that: <blockquote>I can learn anything.</blockquote>`}),
-      ),
-
-      element('section', {}, 
-        element('h2', {textContent: 'EXPERIENCE'}),
       )
     )
   )
